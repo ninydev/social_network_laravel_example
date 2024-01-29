f5a6dcf2409a842dabc23c8d9ed9892eae653ae3
Oleksandr Nykytin <keeper@ninydev.com>
2024-01-29 19:25:41 +0200

    Docs

--- back/api/src/routes/web.php
+++ back/api/src/routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/docs/swagger/', function () {
+    return view('swagger');
+})->name('docs.swagger');
